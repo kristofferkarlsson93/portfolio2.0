1ca860150a8e665763820b0b8a14352c8c91bf7b
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2017-11-26 21:44:34 +0100

    Started to add a fake scroll when user presses arrows

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,72 +1,99 @@
 import React, { Component } from 'react';
 import car from '../img/car.jpg';
 import classNames from 'classnames';
-import { CSSTransitionGroup } from 'react-transition-group' // ES6
+import MaterialIcon from 'react-google-material-icons'
+//import { CSSTransitionGroup } from 'react-transition-group' // ES6
 
 //https://elrumordelaluz.github.io/csshake/
 
 export default class Car extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          isJuggling: false,    
-        }
-      }
-    
-      componentDidMount() {
-        this.initEvents();
-      }
-    
-      initEvents() {
-        document.body.addEventListener('keydown', e => {
-            const left = e.keyCode === 37 || e.which === 37 || e.code === 'ArrowLeft' || e.key === 'ArrowLeft'; 
-            const right = e.keyCode === 39 ||e.which === 39 || e.code === 'ArrowRight' || e.key === 'ArrowRight';
-            const isOkKey = left || right;
-            if(isOkKey) {
-                this.juggle(true);
-            }
-        });
-        document.body.addEventListener('keyup', e => {
-            this.juggle(false);
-        })
-      }
-    
-    juggle(shouldJuggle) {
-        this.setState({
-            isJuggling: shouldJuggle
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldJuggle: false,
+      scrollPos: 0
     }
+  }
+  
+  componentDidMount() {
+    this.initEvents();
+  }
 
-    render() {
-        return (
+  initEvents() {
+    document.body.addEventListener('keydown', e => {
+      const left = e.keyCode === 37 || e.which === 37 || e.code === 'ArrowLeft' || e.key === 'ArrowLeft'; 
+      const right = e.keyCode === 39 ||e.which === 39 || e.code === 'ArrowRight' || e.key === 'ArrowRight';
+      const isOkKey = left || right;
+      if(isOkKey) {
+          this.drive(true);
+      }
+    });
+    document.body.addEventListener('keyup', e => {
+        this.juggle(false);
+    });
+  }
 
-            <CSSTransitionGroup
-            transitionName="example"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <img src={car} className={this.getClassNames()} alt='car' style={styles.carImg}/>
-            
-          </CSSTransitionGroup>
-        );
+  scrollWindow(i) {
+    window.scrollTo(this.state.scrollPos + 10, 0);
+    this.setState({scrollPos: this.state.scrollPos + 10});
+    i ++
+    if(i > 100) {return;}
+  }
+
+  drive(usingKeyboard, right) {
+    this.juggle(true);
+    if (!usingKeyboard) {
+        console.log("prupp");
+        this.scrollWindow();
     }
+  }
 
-    
-    getClassNames() {
-        return classNames({
-            'shake-slow': this.state.isJuggling,
-            'shake-constant': this.state.isJuggling,
-        });
-    }
+
+  stop() {
+    this.juggle(false);
+  }
+  
+  juggle(shouldJuggle) {
+    this.setState({
+        shouldJuggle: shouldJuggle
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <div>
+          <span onMouseDown={ () => this.drive(false, false)} onMouseUp={this.stop.bind(this)} style={styles.arrows}><MaterialIcon icon="keyboard_arrow_left" size={42} /></span>
+          <span onMouseDown={ () => this.drive(false, true)} onMouseUp={this.stop.bind(this)} style={styles.arrows}><MaterialIcon icon="keyboard_arrow_right" size={42}/></span>
+        </div>
+        <img src={car} className={this.getClassNames()} alt='car' style={styles.carImg}/>
+      </div>
+    );
+  }
+
+  
+  getClassNames() {
+    return classNames({
+      'shake-slow': this.state.shouldJuggle,
+      'shake-constant': this.state.shouldJuggle,
+    });
+  }
 }
 
 const styles = {
-    carImg: {
-        maxWidth: 300,
-        bottom: 0,
-        position: 'fixed',
-        left: 100,
-    },
+  container: {
+    bottom: 0,
+    position: 'fixed',
+    left: 100,
+    display: 'grid',
+    gridColumnGap: '100px',
+    gridRowGap: '10px',
+  },
+  arrows: {
+    
+  },
+  carImg: {
+    maxWidth: 300,
+  },
 };
